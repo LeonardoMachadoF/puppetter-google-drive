5847c243bddfb6df835bd8e289a4c66332d3949b
Leonardo Machado <leonardomachado200112@gmail.com>
2022-11-13 15:22:59 -0300

    sec

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs/promises';
+import 'dotenv/config';
+import { chapterStartsIn, searchIn, manga } from './info.js';
 
 const main = async () => {
-    const searchIn = ''
+    for (let i in searchIn) {
+        const urls = [];
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto(searchIn[i], {
+            waitUntil: 'networkidle2'
+        });
 
-    const urls = [];
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(searchIn, {
-        waitUntil: 'networkidle2'
-    });
+        let images = await page.$$('.pmHCK')
 
-    let images = await page.$$('.pmHCK')
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+        for (let i in images) {
+            let allImages = await images[i].$$('img')
+            let res = await allImages[allImages.length - 2].evaluate(i => {
+                return i.getAttribute('src')
+            })
+            urls.push(res.split('=')[0])
+            await page.keyboard.press('ArrowRight')
+        }
 
-    for (let i in images) {
-        // await new Promise((resolve) => setTimeout(resolve, 50))
-        let allImages = await images[i].$$('img')
-        let res = await allImages[allImages.length - 2].evaluate(i => {
-            return i.getAttribute('src')
+        const formatedArray = urls.map((a, index) => {
+            return {
+                imageUrl: a,
+                page: index
+            }
         })
-        urls.push(res.split('=')[0])
-        // await new Promise((resolve) => setTimeout(resolve, 50))
-        await page.keyboard.press('ArrowRight')
-        // await new Promise((resolve) => setTimeout(resolve, 50))
+
+        await fs.writeFile(`${manga.split(' ').join('-').toLowerCase()}-cap-${Number(chapterStartsIn) + Number(i)}.json`, JSON.stringify(formatedArray, null, 2))
+        await browser.close()
     }
-
-    console.log(urls)
-
-    await fs.writeFile('urls.json', JSON.stringify(urls, null, 2))
-    await browser.close()
 }
-
 
 main()
---
+++ info.js
@@ -0,0 +1,6 @@
+export const searchIn = [
+    'https://drive.google.com/drive/u/0/folders/1anKhNlc_7Tl7zafP0iJKtCkr1eRdXmZJ',
+    'https://drive.google.com/drive/u/0/folders/1KdWeRVizfNqnR1nhaWacScubrTogHCit'
+];
+export const chapterStartsIn = 0;
+export const manga = 'Nome do Manga'
